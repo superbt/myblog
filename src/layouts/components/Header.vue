e8796b4a608c5423834387e0039172ef0d24f9f2
bt <dys456123@163.com>
2024-12-10 13:48:11 +0800

    version 前台显示页面梳理

--- src/layouts/components/Header.vue
+++ src/layouts/components/Header.vue
@@ -38,7 +38,7 @@
                                 border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500
                                 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
                                 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="搜索待开发...">
+                                placeholder="搜索待开发...马上就好">
                         </div>
 
                         <button data-collapse-toggle="navbar-search" type="button"
